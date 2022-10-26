d6c4304e491e2021d525b93dc5fe23fe78158e38
Elishama7 <nyecheelishama@gmail.com>
2022-10-25 21:52:33 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print one character out o the two
+ * @str: output
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			-putchar(str[i])
+		else
+			continue;
+	}
+		_putchar('\n');
+}
