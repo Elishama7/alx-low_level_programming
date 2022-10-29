669fb0944c2f6616a61e7d7815e483c6d2140a37
Elishama7 <nyecheelishama@gmail.com>
2022-10-28 17:36:02 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings
  *  @dest: copy to
  *  @src: copy from
- *  Return: Pointer to dest
+ *  Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
